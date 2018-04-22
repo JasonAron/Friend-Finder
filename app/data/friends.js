3ce93a4ceec912964a53768c96f19793e07a70d0
Jason M. Aron <jasonaronart@gmail.com>
2018-04-22 19:40:11 -0400

    Fixed Errors

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,17 @@
-var friends = [
-    {
-    name: "Noam Chomsky",
-    photo: "https://pmcvariety.files.wordpress.com/2016/03/requiem.jpg",
-    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+var friends = [{
+        name: "Noam Chomsky",
+        photo: "https://pmcvariety.files.wordpress.com/2016/03/requiem.jpg",
+        scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
     },
     {
-    name: "Stephen Hawking",
-    photo: "http://www.hawking.org.uk/uploads/1/2/2/1/12210141/swh-1-web.jpg",
-    scores: [1, 1, 2, 2, 3, 3, 4, 4, 5, 5]
+        name: "Stephen Hawking",
+        photo: "http://www.hawking.org.uk/uploads/1/2/2/1/12210141/swh-1-web.jpg",
+        scores: [1, 1, 2, 2, 3, 3, 4, 4, 5, 5]
     },
     {
-    name: "Michio Kaku",
-    photo: "https://www.google.com/imgres?imgurl=http://assets2.bigthink.com/system/user_icons/62750/original/Michio-Kaku-HS.jpgI",
-    scores: [5, 5, 2, 2, 1, 1, 2, 5, 4, 1]
+        name: "Michio Kaku",
+        photo: "https://www.google.com/imgres?imgurl=http://assets2.bigthink.com/system/user_icons/62750/original/Michio-Kaku-HS.jpgI",
+        scores: [5, 5, 2, 2, 1, 1, 2, 5, 4, 1]
     },
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,52 +2,53 @@ var path = require("path")
 
 var friends = require("../data/friends.js")
 
+var app = require("express");
+
 module.exports = function (app, newFriend) {
 
     app.get("/api/friends", function (req, res) {
-        res.json(friends);    
+        res.json(friends);
     });
 
     app.post("/api/friends", function (req, res) {
-        var newFriend = req.body;
-        var newFriendScores = newFriend.scores;
-        var scoresArray = [];
-        var friendsMatch;
-        var differencesArray = [];
-        var bestMatch = 0;
+            var newFriend = req.body;
+            var newFriendScores = newFriend.scores;
+            var scoresArray = [];
+            var friendsMatch;
+            var differencesArray = [];
+            var bestMatch = 0;
 
-        var newUserFriend = {
-            name: newFriend.name,
-            picture: newFriend.picture,
-            scores: []
-        };
-    });
+            var newUserFriend = {
+                name: newFriend.name,
+                picture: newFriend.picture,
+                scores: []
+            };
 
 
-    for (var i = 0; i < newFriendScores.length; i++) {
-        scoresArray.push(parseInt(newFriendScores))
+            // New friends Scores array
+            for (var i = 0; i < newFriendScores.length; i++) {
+                scoresArray.push(parseInt(newFriendScores))
+            };
+            newUserFriend.scores = scoresArray;
+
+            // Differences in scores array
+            for (var i = 0; i < friends.length; i++) {
+                var differences = 0;
+                for (var j = 0; j < newUserFriend.scores.length; j++) {
+                    differences += Math.abs(newUserFriend.scores[j] - friends[i].scores[j]);
+                };
+                differencesArray.push(differences);
+            };
+
+            // Find the best match and set it to the [i] of differencesArray
+            for (var i = 1; i < differencesArray.length; i++) {
+                if (differencesArray[i] <= differencesArray[bestMatch]) {
+                    bestMatch = i;
+                };
+            };
+
+            var bestFriend = friends[bestMatch];
+            res.json(bestFriend);
+            friends.push(newUserFriend);
+        });
     };
-        newUserFriend.scores = scoresArray;
-
-
-    for (var i = 0; i < friends.length; i++) {
-        var differences = 0;
-        for (var j = 0; j < newUserFriend.scores.length; j++) {
-        differences += Math.abs(newUserFriend.scores[j] - friends[i].scores[j]);
-        };
-        differencesArray.push(differences);
-    };
-
-
-    for (var i = 1; i < differencesArray.length; i++) {
-        if(differencesArray[i] <= differencesArray[bestMatch]) {
-            bestMatch = i;
-        };
-    };
-
-
-    var bestFriend = friends[bestMatch];
-    res.json(bestFriend);
-    friends.push(newUserFriend); 
-};
-
